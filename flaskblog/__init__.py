c857645ea78b838d2e67ebd5935fbc450d70337d
tubiky <61419018+tubiky@users.noreply.github.com>
2020-09-26 07:49:49 +0900

    Finished Part 5 - Package Restructure

---
+++ flaskblog/__init__.py
@@ -0,0 +1,18 @@
+# 여기에서 실행되는 모든 것을 flaskblog 라는 package가 된다.
+# 하위 폴더에 존재하는 Module은 flaskblog.forms (= forms.py)
+                            # flaskblog.models (= models.py) 
+                            # flaskblog.routes (= routes.py) 
+# 따라서 import 할 때 from 패키지명 import 컴포넌트의 형식을 사용한다.
+# 예) from flaskblog import app
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '66cd7386a263e5d12cf3acb3c7d2a257'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from flaskblog import routes
